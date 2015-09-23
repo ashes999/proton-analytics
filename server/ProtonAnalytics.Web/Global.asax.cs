831c9a1bd3aa930008b3dbc6a7cff29aa6044112
ashes999 <ashes999@yahoo.com>
2015-09-23 14:27:50 -0400

    Upgrade to MVC5, including upgrading all packages. Also, switched to IIS + SQL Server Express DB

--- server/ProtonAnalytics.Web/Global.asax.cs
+++ server/ProtonAnalytics.Web/Global.asax.cs
@@ -18,7 +18,7 @@ namespace ProtonAnalytics.Web
         {
             AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
