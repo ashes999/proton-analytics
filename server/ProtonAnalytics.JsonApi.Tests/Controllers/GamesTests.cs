49bb06959d59bac407cdd819dccc32a92c821520
ashes999 <ashes999@yahoo.com>
2015-09-29 17:39:51 -0400

    Added tests project (NHtmlUnit 404'd out).

---
+++ server/ProtonAnalytics.JsonApi.Tests/Controllers/GamesTests.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using ProtonAnalytics.JsonApi.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtonAnalytics.JsonApi.Tests.Controllers
+{
+    [TestFixture]
+    class GamesControllerTests
+    {
+        [Test]
+        public void IndexShowsAllGames()
+        {
+            // TODO: how do we do this once we have DI and non-trivial construction?
+            var controller = new GamesController();
+            var actual = controller.Get();
+            Assert.That(actual, Is.Not.Null);
+            Assert.That(actual.Errors, Is.Null);
+            Assert.That(actual.Data, Is.Not.Null);
+
+            var allData = actual.Data;
+            Assert.That(allData.Count, Is.GreaterThan(0));
+
+            var game = allData[0];
+            Assert.That(game.Id, Is.Not.Null);
+            Assert.That(game.Name.Length, Is.GreaterThan(1));
+            Assert.That(game.OwnerId, Is.GreaterThan(0));
+        }
+    }
+}
